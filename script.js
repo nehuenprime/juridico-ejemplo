85424106f42dc6319c84e9d8e5cff2b12e12bf90
Nehuen Boneo <nehuenprime@gmail.com>
2026-08-29 17:58:58 -0300

    completando header

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ function cerrarMenu(){
 menuBtn.addEventListener("click", abrirMenu);
 closeBtn.addEventListener("click", cerrarMenu);
 overlay.addEventListener("click", cerrarMenu);
+
+
+
+
+const navbar = document.querySelector(".navbar-custom");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 20) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
